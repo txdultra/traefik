3baa76dbef229562c2f2ddf55000656cff6680bb
tangxd <ultratxd@gmail.com>
2023-09-12 12:12:17 +0800

    fix bugs

--- pkg/server/middleware/reg/middlewares_register.go
+++ pkg/server/middleware/reg/middlewares_register.go
@@ -10,7 +10,7 @@ type NewFunc func(ctx context.Context, next http.Handler, cfg any, name string) 
 var thirdMiddlewares = make(map[string]NewFunc)
 
 func Register(typeName string, fn NewFunc) {
-	if _, ok := thirdMiddlewares[typeName]; ok {
+	if _, ok := thirdMiddlewares[typeName]; !ok {
 		thirdMiddlewares[typeName] = fn
 	}
 }
